f199bc7c9bc6e494d6c91a396a343e19029f8331
MortenAurs <morten.aursland@gmail.com>
2021-06-13 20:31:59 +0200

    Added dash

--- ansible-vm-with-terraform/ansible-vm.tf
+++ ansible-vm-with-terraform/ansible-vm.tf
@@ -34,7 +34,7 @@ resource "azurerm_virtual_network" "terraformnetwork" {
 
 # Subnet
 resource "azurerm_subnet" "terraformsubnet" {
-    name                 = join("", [var.project_name,"subnet"])
+    name                 = join("", [var.project_name,"-subnet"])
     resource_group_name  = azurerm_resource_group.terraformgroup.name
     virtual_network_name = azurerm_virtual_network.terraformnetwork.name
     address_prefixes       = ["10.0.2.0/24"]
@@ -42,7 +42,7 @@ resource "azurerm_subnet" "terraformsubnet" {
 
 # Public IP
 resource "azurerm_public_ip" "terraformpublicip" {
-    name                         = join("", [var.project_name,"publicIP"])
+    name                         = join("", [var.project_name,"-publicIP"])
     location                     = var.location
     resource_group_name          = azurerm_resource_group.terraformgroup.name
     allocation_method            = "Dynamic"
@@ -54,7 +54,7 @@ resource "azurerm_public_ip" "terraformpublicip" {
 
 # Network security group
 resource "azurerm_network_security_group" "terraformnsg" {
-    name                = join("", [var.project_name, "NSG"])
+    name                = join("", [var.project_name, "-NSG"])
     location            = var.location
     resource_group_name = azurerm_resource_group.terraformgroup.name
 
@@ -77,7 +77,7 @@ resource "azurerm_network_security_group" "terraformnsg" {
 
 # Network interface card
 resource "azurerm_network_interface" "terraformnic" {
-    name                        = join("", [var.project_name, "nic"])
+    name                        = join("", [var.project_name, "-nic"])
     location                    = var.location
     resource_group_name         = azurerm_resource_group.terraformgroup.name
 
@@ -130,7 +130,7 @@ resource "azurerm_linux_virtual_machine" "terraformvm" {
     size                  = "Standard_B2s"
 
     os_disk {
-        name              = join("", [var.project_name, "osDisk"])
+        name              = join("", [var.project_name, "-osDisk"])
         caching           = "ReadWrite"
         storage_account_type = "Premium_LRS"
     }
