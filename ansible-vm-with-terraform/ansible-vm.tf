a9a3b45e3a47726f56df82e0b15c40bc15ed109d
MortenAurs <morten.aursland@gmail.com>
2021-06-13 11:47:04 +0200

    Fixed subnet declaration

--- ansible-vm-with-terraform/ansible-vm.tf
+++ ansible-vm-with-terraform/ansible-vm.tf
@@ -33,7 +33,7 @@ resource "azurerm_virtual_network" "terraformnetwork" {
 }
 
 # Subnet
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "terraformsubnet" {
     name                 = "subnet"
     resource_group_name  = azurerm_resource_group.terraformgroup.name
     virtual_network_name = azurerm_virtual_network.terraformnetwork.name
