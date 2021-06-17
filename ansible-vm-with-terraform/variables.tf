722f817d3e292555c1c0a3fbbf494f6aaa604127
Ove Aursland <jeircul@mailbox.org>
2021-06-17 16:20:06 +0200

    added ssh testkey

--- ansible-vm-with-terraform/variables.tf
+++ ansible-vm-with-terraform/variables.tf
@@ -22,5 +22,5 @@ variable "user_name"  {
 
 variable "public_key"  {
     type = string
-    default = "<INSERT PUBLIC KEY HERE>"
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6wWnNGaOxZkrUyTJKCBUnvydhbLUInn+qNTyLCWoD7xBhkhyS/MV7Tsi+mA06czVyPIDLQBQ+eV2lS+8ti14ahV/RyKEtRu34PjPg/G+Zwt4r5L2KtEjj0QGExeqzSYGzpog3DrgS9BNABHxhW4WDeuaxD+UsXrhUoHEhOqoIkWg7qXD8NXnIsMsQ2a0BHnq1D01A3of4qmvP5fpttpm/IGAqk7RGJDrKfeS7+ySdqu8/S+Hun0xBwB8kHtI1qo8gVrsEOU1tR42nNGVACDgUwrmGnKeTWzqjlpvhNHdHnrcu860eroz90oGQu8rRaw4ifwX5dXfbskGNZN4ImYm8DFNP7YKCoa5PSSZiVZZb31kWtso2eTNsN3u958UJRqoHtFa0DlTceoFmfX4wAkBXn6XSzQZL2Hja14vklLUJC8LnjVlT6BWTbre3DhJ6AJnmeF1P+yVxP5Uza/L6KrjlkQwc7uzbM3g8e+oskHBM3Xd8slR7ozUHdW6E8Ld5IeE= generated-by-azure"
 }
